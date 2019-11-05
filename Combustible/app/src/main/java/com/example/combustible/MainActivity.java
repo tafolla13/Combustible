605f16633b2a4309bcfcc1c4663c6f6ccb57c3e3
Tafolla <tafolla.1309@gmail.com>
2019-11-05 13:10:33 -0600

    CREACION DE CRONOMETRO, PRIMER VISTA

---
+++ Combustible/app/src/main/java/com/example/combustible/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.combustible;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showSelectedFragment(new combustible());
+    }
+    private void showSelectedFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit();
+    }
+}
